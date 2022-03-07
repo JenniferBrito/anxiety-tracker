c771b8fcfddb9aed533914a12ac40bde7c825222
JenniferBrito <jennifer.brito3@gmail.com>
2022-03-07 18:01:10 -0300

    Added Firestore configurations

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:anxiety_tracker/pages/crisis_entrys.dart';
 import 'package:anxiety_tracker/pages/new_page_screen.dart';
 import 'package:flutter/material.dart';
-
-void main() {
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
